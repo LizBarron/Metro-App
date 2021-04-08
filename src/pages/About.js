cd4330c11a6740272bdd95b57951950ee189de8d
Liz Barron <50109362+LizBarron@users.noreply.github.com>
2021-04-08 10:27:17 -0500

    added metroapp routing to href

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
 
             <div className="container-fluid" style={{ fontSize: 20, paddingTop: 20 }}>
                 <p>Hi, this website was created by Elizabeth Barron using the <a href="https://svc.metrotransit.org/swagger/index.html?urls.primaryName=NexTrip%20API%20-%20v2" >NexTrip API</a>.</p>
-                <p>You can find my Github repo at <a href="https://www.github.com/lizbarron">https://www.github.com/lizbarron/metro-app</a>.</p>
+                <p>You can find my Github repo at <a href="https://www.github.com/lizbarron/metro-app">https://www.github.com/lizbarron/metro-app</a>.</p>
             </div>
         </>
     )
